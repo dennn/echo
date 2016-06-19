b25844d8da4bc597dc37ada11711cf94cace0601
Denis Ogun <do1303@my.bristol.ac.uk>
2016-06-19 19:32:58 +0100

    Add the UITableViewController

---
+++ Echo/UsersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UsersViewController.swift
+//  Echo
+//
+//  Created by Denis Ogun on 19/06/2016.
+//  Copyright © 2016 Denis Ogun. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UITableViewController {
+
+}
